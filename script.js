3e2da337c04a84cac5f775a565791948d3fc8e2c
JacksonWeudes <weudes2024jackson@gmail.com>
2025-07-05 10:07:19 -0300

    mais ajustes

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+// pegando elementos do menu no DOM
 const menuBtn = document.getElementById("menu-icon")
 const sideMenu = document.getElementById("side-menu")
 const overlay = document.getElementById("overlay")
 
+// função toggle para ativar/desativar o menu
 menuBtn.addEventListener("click", () => {
-    sideMenu.classList.toggle("open")
-    menuBtn.classList.toggle("open")
-    overlay.classList.toggle("open")
-    document.body.classList.toggle("open")
+    sideMenu.classList.toggle("open") // abre o menu lateral
+    menuBtn.classList.toggle("open")  // tranforma o icone do menu para um X
+    overlay.classList.toggle("open")  // ativa o overlay com filtro de blur
+    document.body.classList.toggle("open") // impede a rolagem vertical enquanto o menu estiver aberto
 })
 
+// buscando os botões dos cards da área "explorar"
 const btn1 = document.getElementById("btn1")
 const btn2 = document.getElementById("btn2")
 const btn3 = document.getElementById("btn3")
 
+// usando arrow functions para cada um dos botões levar para em endereço específico
 btn1.addEventListener("click", () => {
     window.location.href = "paginas/arquitetura.html"
 })
@@ -25,11 +29,12 @@ btn3.addEventListener("click", () => {
     window.location.href = "paginas/exemplos.html"
 })
 
+// animação simples dos cards na área explorar usando GSAP
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger)
 
     gsap.from(".card", {
-        opacity: 0,
+        opacity: 0,         
         duration: 4,
         scrollTrigger: {
             trigger: ".card",
